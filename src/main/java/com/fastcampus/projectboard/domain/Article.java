08306f7f16012c9750c978a2e6938007a8c29ff8
hyebin <xxico89@gmail.com>
2023-11-25 01:56:32 +0900

    #12 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계
    생성자와 `equals()`, `hashcode()` 등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,15 +1,86 @@
 package com.fastcampus.projectboard.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+// 모든 필드는 접근이 가능해야한다.
+// @Table = Entity 클래스와 매핑되는 데이터베이스 테이블에 대한 설정 지정
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+// JPA를 사용하여 entity로 변경
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+    @Id                 // 엔터티 클래스의 primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)             // DB가 자동으로 ID 생성
     private Long id;
-    private String title;       // 제목
-    private String content;     // 본문
-    private String hashtag;     // 해시태그
 
-    private LocalDateTime createdAt;            // 생성일시
-    private String createdBy;                   // 생성자
-    private LocalDateTime modifiedAt;           // 수정일시
-    private String modifiedBy;                  // 수정자
+    // 내용을 변경할 수 있으므로 setter 설정
+    // null이 가능한가 -> true가 default 값 => @Column 이라 표기 가능하지만, 파라미터 없이 쓴다면 생략도 가능!
+    // @Column = 엔터티 클래스의 필드와 데이터베이스 테이블의 컬럼 매핑
+    @Setter @Column(nullable = false) private String title;       // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content;     // 본문
+
+    // @Column 으로 감지
+    @Setter private String hashtag;     // 해시태그
+
+    @ToString.Exclude                   // ToString 끊어주기
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    // auto setting (JPA Auditing)
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;            // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;                   // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;           // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;                  // 수정자
+
+
+    // entity는 public 또는 protected로 아무 인자없는 생성자가 필요합니다.
+    protected Article() {
+
+    }
+
+    // 생성자가 private으로 선언되어 있으므로 외부에서 직접 객체 생성 X → of 메서드를 통해 객체 생성 유도
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+
+    // equals와 hashCode 메서드를 통해 객체의 동등성 비교
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;              // pattern variable
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
